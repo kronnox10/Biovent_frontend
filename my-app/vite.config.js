2e0a3c0d8cece9e2eb81d024385a942c069a5607
kronnox10 <134155626+kronnox10@users.noreply.github.com>
2024-12-24 16:31:14 -0500

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,10 +3,5 @@ import svelte from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  root: './my-app',  // Asegúrate de que esté apuntando al directorio correcto
-  build: {
-    rollupOptions: {
-      input: './my-app/src/app.html'  // Ajusta la ruta a tu archivo HTML
-    }
-  }
+  root: './frontend',  // Apunta al directorio correcto si tienes subdirectorios
 });
