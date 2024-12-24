933e1a795f34a0b8db4b5aaa510cb0a302aaabb2
kronnox10 <134155626+kronnox10@users.noreply.github.com>
2024-12-24 16:24:52 -0500

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
- //que me habias dicho que hiciera que fui por agua
   
 export default defineConfig({
-	plugins: [sveltekit()],
-  });
-  // http://192.168.56.1:5173/
+  plugins: [svelte()],
+  root: './my-app',  // Apunta al directorio de tu aplicación
+  build: {
+    rollupOptions: {
+      input: './src/app.html' // Especifica tu archivo de entrada aquí
+    }
+  }
+});
+
+
