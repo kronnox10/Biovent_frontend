dec6d4cf4ba861112de298f07c0b1ada4d7439f1
Jesus Coronado <jesusalfredocoronado@gmail.com>
2024-12-26 14:05:05 -0500

    frontend v2.0.10

--- my-app/src/routes/servicios/+page.svelte
+++ my-app/src/routes/servicios/+page.svelte
@@ -13,13 +13,18 @@
     <div class="container" style="">
         <div class="card mb-4" id="preventivo" style="max-width: 100vw; ">
             <div class="row g-0 py-2 px-2" style="border-radius: 15%;">
-                <div class="col-md-4">
-                    <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" 
-                    class="img-fluid rounded-start w-100" 
-                    alt="..."
-                    width="750"
-                    height="550"
-                    >
+                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade col-md-4" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner ">
+                        <div class="carousel-item active">
+                            <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" class="img-fluid rounded-start" alt="..." width="750" height="550">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/capacitacion_personal.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/diseño_prototipomedico.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-8">
                     <div class="card-body" >
@@ -38,27 +43,37 @@
                         <p class="card-text py-4">Organizamos un plan personalizado para el cuidado regular de sus dispositivos, asegurando su disponibilidad y confiabilidad.</p>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <img src="/servicios/cronograma_mantenimiento.png" 
-                    class="img-fluid rounded-start w-100" 
-                    alt="..."
-                    width="750"
-                    height="550"
-                    >
+                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade col-md-4" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner ">
+                        <div class="carousel-item active">
+                            <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" class="img-fluid rounded-start" alt="..." width="750" height="550">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/capacitacion_personal.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/diseño_prototipomedico.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div> 
 
         <div class="card mb-4 mt-5" id="capacitacion" style="max-width: 100vw;">
             <div class="row g-0 py-2 px-2">
-            <div class="col-md-4">
-                <img src="/servicios/capacitacion_personal.png" 
-                class="img-fluid rounded-start w-100" 
-                alt="..."
-                width="750"
-                height="550"
-                >
-            </div>
+                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade col-md-4" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner ">
+                        <div class="carousel-item active">
+                            <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" class="img-fluid rounded-start" alt="..." width="750" height="550">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/capacitacion_personal.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/diseño_prototipomedico.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                    </div>
+                </div>
             <div class="col-md-8">
                 <div class="card-body">
                 <h5 class="card-title py-2" >Capacitacion personal medico - asistencial</h5>
@@ -77,26 +92,27 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <img src="/servicios/diseño_prototipomedico.png" 
-                    class="img-fluid rounded-start w-100" 
-                    alt="..."
-                    width="750"
-                    height="550"
-                    >
+                    <!-- svelte-ignore a11y_media_has_caption -->
+                    <video src="/servicios/diseño_prototipomedico.mp4" class="w-100" title="dp" autoplay loop></video>
                 </div>
             </div>
         </div> 
 
         <div class="card mb-4 mt-5" id="ventas" style="max-width: 100vw;">
             <div class="row g-0 py-2 px-2">
-            <div class="col-md-4">
-                <img src="/servicios/venta_equipos.png" 
-                class="img-fluid rounded-start w-100" 
-                alt="..."
-                width="750"
-                height="550"
-                >
-            </div>
+                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade col-md-4" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner ">
+                        <div class="carousel-item active">
+                            <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" class="img-fluid rounded-start" alt="..." width="750" height="550">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/capacitacion_personal.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/diseño_prototipomedico.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                    </div>
+                </div>
             <div class="col-md-8">
                 <div class="card-body">
                 <h5 class="card-title py-2" >Venta y Alquiler de Dispositivos Médicos:</h5>
@@ -114,13 +130,18 @@
                         <p class="card-text py-4">Creamos herramientas para entrenamientos clínicos, ayudando a perfeccionar habilidades en un entorno seguro y controlado.</p>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <img src="/servicios/simulacion.png" 
-                    class="img-fluid rounded-start w-100" 
-                    alt="..."
-                    width="750"
-                    height="550"
-                    >
+                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade col-md-4" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner ">
+                        <div class="carousel-item active">
+                            <img src="/servicios/Mantenimiento_preventivo-correctivo1.png" class="img-fluid rounded-start" alt="..." width="750" height="550">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/capacitacion_personal.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/servicios/diseño_prototipomedico.png" class="img-fluid rounded-start"  width="750" height="550" alt="...">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div> 
