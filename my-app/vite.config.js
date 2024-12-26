b73e4c4ea859a608053ef5075e156a66c13421f5
Jesus Coronado <jesusalfredocoronado@gmail.com>
2024-12-26 09:43:25 -0500

    vite_config_fix

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,12 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
-  root: './my-app',  // Apunta al directorio donde está tu app.html y src/
-  build: {
-    rollupOptions: {
-      input: './my-app/src/app.html',  // Especificamos el archivo HTML de entrada
-    }
-  }
+	plugins: [sveltekit()]
 });
