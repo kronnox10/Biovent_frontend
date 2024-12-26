1a379fe78eb1752a101e35a64ff802bbb8886b6f
Jesus Coronado <jesusalfredocoronado@gmail.com>
2024-12-26 09:42:34 -0500

    svelt_config-fix

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+/*import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+/**const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -11,3 +11,12 @@ const config = {
 };
 
 export default config;
+*/
+import adapter from '@sveltejs/adapter-node';
+
+export default {
+  kit: {
+    // other config options
+    adapter: adapter(),  // Agrega el adaptador para Node.js
+  }
+};
